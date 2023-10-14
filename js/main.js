f12891182118982ce33ab72c2dc91eeb06b81c98
sulthn <ASUS@WIRELESSHOSE>
2023-10-14 15:03:28 +0700

    spin on/off switch + mobile functionality + ratio + L + youre bald

--- js/main.js
+++ js/main.js
@@ -1,30 +1,64 @@
 import * as THREE from 'three';
-import * as BufferGeometryUtils from 'three/addons/utils/BufferGeometryUtils.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
-let scene, camera, renderer, dna;
+let scene, camera, renderer, controls;
+let dna;
+let spin = true;
 const p = new THREE.Object3D();
-const count = 20;
+const count = 30;
 
 window.addEventListener('resize', onWindowResize);
+$("#description-btn").on( "click", (e) => {
+	$(".icon").css('display', 'none');
+	$(".window").css('display', 'inline-block');
+});
+
+$("#spin-btn").on("click", (e) => {
+	if (e.target.classList.toggle("active")) {
+		spin = true;
+	}
+	else {
+		spin = false;
+	}
+});
+
+$("#close-window").on("click", (e) => {
+	$(".icon").css('display', 'initial');
+	$(".window").css('display', 'none');
+});
+
 init();
 animate();
 
 function init() {
-	camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 	scene = new THREE.Scene();
-	
-	renderer = new THREE.WebGLRenderer({ canvas: mainScreen, alpha: true });
 
-	renderer.setSize( window.innerWidth, window.innerHeight );
+	renderer = new THREE.WebGLRenderer({ canvas: mainScreen, alpha: true, antialias: true });
+	renderer.setSize(document.documentElement.clientWidth, document.documentElement.clientHeight);
 	renderer.shadowMap.enabled = true
 
-	const ambience = new THREE.AmbientLight( 0x404040, 10 );
+	camera = new THREE.PerspectiveCamera( 75, document.documentElement.clientWidth / document.documentElement.clientHeight, 0.1, 1000 );
+
+	camera.position.set(0, 0, 30);
+
+	controls = new OrbitControls( camera, renderer.domElement )
+	controls.listenToKeyEvents( window );
+	controls.enablePan = false;
+	controls.enableDamping = true;
+	controls.dampingFactor = 0.05;
+	controls.screenSpacePanning = false;
+	controls.minDistance = 10;
+	controls.maxDistance = 100;
+	controls.maxPolarAngle = Math.PI;
+
+	const ambience = new THREE.AmbientLight( 0x808080, 10 );
 	scene.add( ambience );
 
 	const light = new THREE.PointLight( 0xffffff, 100, 100 );
 	light.position.set( 5, 5, 5 );
 	light.castShadow = true;
 	scene.add( light );
+
 	/*
 	const groundplane = new THREE.Mesh(new THREE.BoxGeometry(10, 0.01, 10), new THREE.MeshPhongMaterial({color:0xffffff}));
 	groundplane.rotation.y = Math.PI/2;
@@ -33,68 +67,99 @@ function init() {
 	scene.add(groundplane);
 	*/
 
-	let geometry;
+	const back1 = new THREE.InstancedMesh(
+		new THREE.SphereGeometry(0.5, 64, 16).translate(2.5, 0, 0), 
+		new THREE.MeshLambertMaterial({color:0xa3a3a3}), 
+		count
+	);
+	back1.castShadow = true;
 
-	geometry = new THREE.SphereGeometry(0.5, 64, 16);
-	geometry.translate(2.5, 0, 0);
-	const base1 = new THREE.InstancedMesh(geometry, new THREE.MeshPhongMaterial({color:0xff0000}), count);
+	const back2 = new THREE.InstancedMesh(
+		new THREE.SphereGeometry(0.5, 64, 16).translate(-2.5, 0, 0), 
+		new THREE.MeshLambertMaterial({color:0xa3a3a3}), 
+		count
+	);
+	back2.castShadow = true;
+
+	const base1 = new THREE.InstancedMesh(
+		new THREE.CylinderGeometry(0.25, 0.25, 2.5, 64).rotateZ(Math.PI/2).translate(-1.25, 0, 0), 
+		new THREE.MeshLambertMaterial(), 
+		count);
 	base1.castShadow = true;
 
-	geometry = new THREE.SphereGeometry(0.5, 64, 16);
-	geometry.translate(-2.5, 0, 0);
-	const base2 = new THREE.InstancedMesh(geometry, new THREE.MeshPhongMaterial({color:0x0000ff}), count);
+	const base2 = new THREE.InstancedMesh(
+		new THREE.CylinderGeometry(0.25, 0.25, 2.5, 64).rotateZ(Math.PI/2).translate(1.25, 0, 0), 
+		new THREE.MeshLambertMaterial(), 
+		count);
 	base2.castShadow = true;
 
-	geometry = new THREE.CylinderGeometry(0.25, 0.25, 5, 64);
-	geometry.rotateZ(Math.PI/2);
-	const bases = new THREE.InstancedMesh(geometry, new THREE.MeshPhongMaterial({color:0x9b9b9b}), count);
-	bases.castShadow = true;
-
-	dna = [base1, base2, bases]
+	dna = [back1, back2, base1, base2];
 
 	scene.add(dna[0]);
 	scene.add(dna[1]);
 	scene.add(dna[2]);
+	scene.add(dna[3]);
 
-	camera.position.z = 25;
-}
+	let color = new THREE.Color(0xffffff);
 
-function render() {
-
-	const time = Date.now() * 0.001;
-
-	const offset = (count - 1)/2
-
+	const baseColor = [0xeb4b4b, 0x5151d1, 0xfffd6e, 0x23e323];
+	let rand;
 	for (let c = 0; c < count; c++) {
-		p.position.set(0, offset - c, 0);
-		p.rotation.y = (c % 10) * (36 * (Math.PI/180)) + time;
-		console.log((c % 10) * (36 * (Math.PI/180)) + time);
-		p.updateMatrix();
-
-		dna[0].setMatrixAt(c, p.matrix);
-		dna[1].setMatrixAt(c, p.matrix);
-		dna[2].setMatrixAt(c, p.matrix);
+		rand = Math.floor(Math.random() * baseColor.length);
+		color = new THREE.Color(baseColor[rand]);
+		dna[2].setColorAt(c, color);
+		dna[2].instanceColor.needsUpdate = true;
+		color = new THREE.Color(baseColor[3 - rand]);
+		dna[3].setColorAt(c, color);
+		dna[3].instanceColor.needsUpdate = true;
 	}
-
-	dna[0].instanceMatrix.needsUpdate = true;
-	dna[0].computeBoundingSphere();
-	dna[1].instanceMatrix.needsUpdate = true;
-	dna[1].computeBoundingSphere();
-	dna[2].instanceMatrix.needsUpdate = true;
-	dna[2].computeBoundingSphere();
-
-	renderer.render( scene, camera );
 }
 
 function animate() {
 	requestAnimationFrame( animate );
 
+	controls.update();
+
 	render();
 }
 
+var tracktime = 0;
+var offsettime = 0;
+var time = 0;
+
+function render() {
+	tracktime = time;
+
+	if (spin) {
+		time = Date.now() * 0.001 - offsettime;
+	}
+	else {
+		offsettime = Date.now() * 0.001 - tracktime;
+	}
+
+	const offset = (count - 1)/2
+
+	for (let c = 0; c < count; c++) {
+		p.position.set(0, offset - c, 0);
+		p.rotation.y = (c % 10) * (36 * (Math.PI/180)) + tracktime;
+		p.updateMatrix();
+
+		for (let x = 0; x < dna.length; x++) {
+			dna[x].setMatrixAt(c, p.matrix);
+		}
+	}
+
+	for (let x = 0; x < dna.length; x++) {
+		dna[x].instanceMatrix.needsUpdate = true;
+		dna[x].computeBoundingSphere();
+	}
+
+	renderer.render( scene, camera );
+}
+
 function onWindowResize() {
-	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.aspect = document.documentElement.clientWidth / document.documentElement.clientHeight;
     camera.updateProjectionMatrix();
 
-    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setSize(document.documentElement.clientWidth, document.documentElement.clientHeight);
 }
